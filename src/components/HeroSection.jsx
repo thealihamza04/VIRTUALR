47c57d57c975462d1b56cb80757a721ca1d6ff38
Ali Hamza <ah0681988@gmail.com>
2025-05-07 05:25:22 +0500

    fix ui issues

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,29 +1,57 @@
-import Video1 from '../assets/video1.mp4'
-import Video2 from '../assets/video2.mp4'
+import Video1 from "../assets/video1.mp4";
+import Video2 from "../assets/video2.mp4";
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col items-center mt-6 lg:mt-20">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+    <div className='flex flex-col items-center mt-6 lg:mt-20'>
+      <h1 className='text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide'>
         VirtualR build tools
-        <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+        <span className='bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text'>
           {" "}
           for developers
         </span>
       </h1>
-      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-        Empower your creativity and bring your VR app ideas to life with our intuitive development tools. Get started today and turn your imagination into immersive reality!
+      <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
       </p>
-      <div className="flex justify-center my-10">
-        <a href="#" className="bg-gradient-to-r from-orange-500 to-red-800 py-3 px-4 mx-3 rounded-md">Start for free</a>
-        <a href="#" className="py-3 px-4 mx-3 border rounded-md">Documentation</a>
+      <div className='flex justify-center my-10'>
+        <a
+          href='#'
+          className='bg-gradient-to-r from-orange-500 to-red-800 py-3 px-2 text-xs md:text-lg md:px-4 mx-0 rounded-md   leading-7'
+        >
+          Start free
+        </a>
+        <a
+          href='#'
+          className='py-3 px-4 mx-3 border rounded-md text-xs md:text-lg leading-7'
+        >
+          Documentation
+        </a>
       </div>
-      <div className="flex mt-10 justify-center">
-        <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"> <source src={Video1} type='video/mp4' /> </video>
-        <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"> <source src={Video2} type='video/mp4' /> </video>
+      <div className='flex mt-10 justify-center'>
+        <video
+          autoPlay
+          loop
+          muted
+          className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'
+        >
+          {" "}
+          <source src={Video1} type='video/mp4' />{" "}
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'
+        >
+          {" "}
+          <source src={Video2} type='video/mp4' />{" "}
+        </video>
       </div>
     </div>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
--- src/components/NAVBAR.jsx
+++ src/components/NAVBAR.jsx
@@ -1,56 +1,78 @@
-import Logo from '../assets/logo.png'
-import { useState } from 'react'
-import { Menu, X } from 'lucide-react'
-import { navItems } from '../constants/index'
+import Logo from "../assets/logo.png";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { navItems } from "../constants/index";
 
 const NAVBAR = () => {
-    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
-    const toggleNavbar = () => {
-        setMobileDrawerOpen(!mobileDrawerOpen);
-    }
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
 
-    return (
-        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/800">
-            <div className="container py-4 mx-auto relative text-sm">
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center flex-shrink-0">
-                        <img className="h-10 w-10 mr-2" src={Logo} alt="Logo" />
-                        <span className="text-xl tracking-tight">VirtualR</span>
-                    </div>
-                    <ul className="hidden lg:flex ml-14 space-x-12">
-                        {navItems.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    <a href={item.href}>{item.label}</a>
-                                </li>)
-                        })}
-                    </ul>
-                    <div className="hidden lg:flex justify-center space-x-12 items-center">
-                        <a href="#" className="py-2 px-3 border rounded-md">Sign In</a>
-                        <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md">Create an account</a>
-                    </div>
-                    <div className="lg:hidden md:flex flex-col justify-end">
-                        <button  className="pr-3" onClick={toggleNavbar}>{mobileDrawerOpen ? <X /> : <Menu />}</button>
-                    </div>
-                </div>
-                {mobileDrawerOpen && (
-                    <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
-                        <ul>
-                            {navItems.map((item, index) => (
-                                <li key={index} className="py-4">
-                                    <a href={item.href}>{item.label}</a>
-                                </li>
-                            ))}
-                        </ul>
-                        <div className="flex space-x-6">
-                            <a href="#" className="py-2 px-3 border rounded-md">Sign In</a>
-                            <a href="#" className="px-3 py-2 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">Create an account</a>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className='sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/800'>
+      <div className='container py-4 mx-auto relative text-sm z-50 '>
+        <div className='flex justify-between items-center '>
+          <div className='flex items-center flex-shrink-0'>
+            <img className='h-10 w-10 mr-2' src={Logo} alt='Logo' />
+            <span className='text-xl tracking-tight'>VirtualR</span>
+          </div>
+          <ul className='hidden lg:flex ml-14 space-x-12'>
+            {navItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <div className='hidden lg:flex justify-center space-x-12 items-center'>
+            <a href='#' className='py-2 px-3 border rounded-md'>
+              Sign In
+            </a>
+            <a
+              href='#'
+              className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'
+            >
+              Create an account
+            </a>
+          </div>
+          <div className='lg:hidden md:flex flex-col justify-end'>
+            <button className='pr-3' onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`fixed right-0 z-20 bg-neutral-900 w-full p-12 flex ${
+            mobileDrawerOpen ? "top-12 opacity-100" : "-top-96 opacity-0"
+          } transition-all duration-150 ease-in flex-col justify-center items-center -z-10 lg:hidden `}
+        >
+          <ul>
+            {navItems.map((item, index) => (
+              <li key={index} className='py-4'>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className='flex space-x-2 md:space-x-6'>
+            <a
+              href='#'
+              className='py-2 px-2 border rounded-md text-xs md:text-md'
+            >
+              Sign In
+            </a>
+            <a
+              href='#'
+              className='px-2 py-2 rounded-md bg-gradient-to-r from-orange-500 to-orange-800'
+            >
+              Create an account
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default NAVBAR
+export default NAVBAR;
